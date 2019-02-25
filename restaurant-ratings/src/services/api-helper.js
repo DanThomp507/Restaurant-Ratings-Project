376b324a6ef017572f873b5b34772505f494365e
Daniel Thompson <DanThomp507@gmail.com>
2019-02-25 09:47:00 -0500

    cleared out react app and finished my API helper file

---
+++ restaurant-ratings/src/services/api-helper.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const BASE_URL = 'https://data.cityofnewyork.us/resource/9w7m-hzhe/';
+
+const fetchRestaurants = async () => {
+  try {
+    const resp = await axios(`${BASE_URL}`)
+    console.log(resp);
+    return resp.data;
+  } catch(e) {
+    console.log('got a problem: ', e);
+    return [];
+  }
+}
+export { fetchRestaurants }
