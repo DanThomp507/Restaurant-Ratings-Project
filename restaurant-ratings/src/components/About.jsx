856f3be13c455a226dac0e5a91707ee0e3847589
Daniel Thompson <DanThomp507@gmail.com>
2019-02-27 12:32:08 -0500

    more styling updates

--- restaurant-ratings/src/components/About.jsx
+++ restaurant-ratings/src/components/About.jsx
@@ -4,9 +4,17 @@ const About = () => {
   return (
     <div className="about">
       <h1 className="about-header"> Why do restaurant grades matter?</h1>
+        <article className="about-article">
       <p className="about-p">There are over 24,000 restaurants in New York City.
-      Therefore, it is critically important to monitor the quality of 
+      Therefore, it is critically important to monitor their quality in order
+      to ensure they are complying with health and safety standards.
       </p>
+      <p className="about-p">This application makes it easy for New Yorkers to
+      search for any one of these restaurants-both by name and phone number.
+      Furthermore, you can search by zipcode to examine the general quality and
+      the inspection history of the restaurants in that particular area.
+      </p>
+        </article>
     </div>
   )
 }
