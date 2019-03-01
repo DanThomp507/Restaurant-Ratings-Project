a0734a5b86d5fc25567f82b38ae8217351b92126
Daniel Thompson <DanThomp507@gmail.com>
2019-02-28 20:48:02 -0500

    updated .env for google maps

--- restaurant-ratings/src/components/Map.jsx
+++ restaurant-ratings/src/components/Map.jsx
@@ -1,10 +1,12 @@
 import GoogleMaps from "simple-react-google-maps"
 import React from 'react';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 const Map = (props) => {
     return(
       <GoogleMaps
-        apiKey={"AIzaSyBEOd9mJ7SqTUbU0_LievpjB8m9A7ScxQw"}
+        apiKey={API_KEY}
         style={{height: "500px", width: "100%"}}
         zoom={14}
         center={{lat: 40.740986, lng: -73.990293}}
