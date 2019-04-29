b7638735dd4d872a563b4757792f2b4e1ae09e0c
Daniel Thompson <DanThomp507@gmail.com>
2019-04-28 21:19:49 -0400

    updated map component

--- restaurant-ratings/src/components/Map.jsx
+++ restaurant-ratings/src/components/Map.jsx
@@ -1,74 +1,32 @@
-import React, { Component } from 'react';
-import  GoogleMapReact from 'google-map-react';
-import {fetchYelpData} from '../services/api-helper.js';
+import React, { Component } from "react";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-const API_KEY = process.env.REACT_APP_API_KEY;
-
-
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 40.740758,
-      lng: -73.990164
-    },
-    zoom: 13
-  };
-  constructor(props){
+export class RestaurantMap extends Component {
+  constructor(props) {
     super(props);
-  this.state ={
-    mapSearch: ''
-  }
-  this.handleChange = this.handleChange.bind(this);
-  }
-  async handleSubmit(id){
-    try {
-      let yelp = await fetchYelpData(id);
-      this.setState({
-        mapSearch: yelp
-      })
-    }
-    catch(error){
-      console.error(error);
-    }
+    this.state = {
+      selectedPlace: {}
+    };
   }
 
- handleChange(event) {
-  this.setState({
-    mapSearch: event.target.value
-  });
-}
+
+
   render() {
-    return (
-        <div style={{ height: '100vh', width: '100%' }}>
-        <h1 className="map-header">Map the City</h1>
-        <form className="map-form"onSubmit = {(event) => {
-          event.preventDefault();
-          this.handleSubmit(this.state.mapSearch)}
-        }>
-        <input type="text"
-               className="input"
-               placeholder="Enter a Restaurant Name"
-               name="name"
-               value={this.state.mapSearch}
-               onChange={this.handleChange}
-               />
-        <button className="map-button">Search</button>
-        </form>
-                <GoogleMapReact
-                  bootstrapURLKeys={{ key: `${API_KEY}` }}
-                  defaultCenter={this.props.center}
-                  defaultZoom={this.props.zoom}
-                >
-                  <AnyReactComponent
-                    lat={40.740758}
-                    lng={-73.990164}
-                    markers={{lat: 40.740986, lng: -73.990293}}
-                  />
-                </GoogleMapReact>
-                </div>
-            );
-          }
-        }
 
-        export default SimpleMap;
+    return (
+      <Map
+        style={{ width: "100%", height: "80vh" }}
+        center={{ lat: 40.730610,  lng: -73.935242}}
+        onClick={this.onMapClicked}
+        google={this.props.google}
+        centerAroundCurrentLocation={true}
+        zoom={12}
+      >
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_APIKEY
+})(RestaurantMap);
