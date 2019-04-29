c86216102ad8fe400e54d74ac126936bbc6eadac
Daniel Thompson <DanThomp507@gmail.com>
2019-04-28 21:36:39 -0400

    fixed background image

--- restaurant-ratings/src/components/Map.jsx
+++ restaurant-ratings/src/components/Map.jsx
@@ -5,7 +5,7 @@ export class RestaurantMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPlace: {}
+      selectedPlace: {},
     };
   }
 
@@ -16,11 +16,12 @@ export class RestaurantMap extends Component {
     return (
       <Map
         style={{ width: "100%", height: "80vh" }}
-        center={{ lat: 40.730610,  lng: -73.935242}}
         onClick={this.onMapClicked}
         google={this.props.google}
-        centerAroundCurrentLocation={true}
-        zoom={12}
+        zoom={14}
+        centerAroundCurrentLocation={false}
+        center={{lat: 40.785091, lng: -73.968285}}
+        markers={{lat: 40.740986, lng: 73.990293}}
       >
       </Map>
     );
